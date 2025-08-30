74b92023330f37c0dacb8e55d1524130d8bce67a
alw7024-jpg <alw7024@gmail.com>
2025-08-30 12:27:52 -0500

    init commit

---
+++ polls/static/polls/main.js
@@ -0,0 +1,7 @@
+async function test_function() {
+    document.getElementById('test_message').innerText = 'Test button clicked.';
+    document.getElementById('test_btn').disabled = true;
+    document.getElementById('test_btn').innerText = 'done';
+    document.getElementById('testing').disabled = true;
+    document.getElementById('testing').innerText = 'done';
+}
